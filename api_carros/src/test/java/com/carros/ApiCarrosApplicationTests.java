adba742607d23c3e73b14209a3731a60f3d3e3a7
Wallace Gonçalves <wallacecosme@gmail.com>
2021-04-07 21:01:43 -0300

    Project created

---
+++ api_carros/src/test/java/com/carros/ApiCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
