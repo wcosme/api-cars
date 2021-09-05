c9ab5276281465da1054ac02d14276555961adde
Wallace Gonçalves <wallacecosme@gmail.com>
2021-09-05 20:46:21 -0300

    Implementando testes unitários

--- api_carros/src/test/java/com/carros/test/CarrosAPITest.java
+++ api_carros/src/test/java/com/carros/test/CarrosAPITest.java
@@ -3,11 +3,15 @@ package com.carros.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -30,6 +34,15 @@ public class CarrosAPITest {
     private ResponseEntity<CarroDTO> getCarro(String url) {
         return
                 rest.withBasicAuth("user","123").getForEntity(url, CarroDTO.class);
+    }
+    
+    private ResponseEntity<List<CarroDTO>> getCarros(String url) {
+        return rest.withBasicAuth("user","123").exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CarroDTO>>() {
+                });
     }
 	
 	@Test
@@ -60,5 +73,39 @@ public class CarrosAPITest {
         // Verificar se deletou
         assertEquals(HttpStatus.NOT_FOUND, getCarro(location).getStatusCode());
     }
+	
+	 @Test
+	    public void testLista() {
+	        List<CarroDTO> carros = getCarros("/api/v1/carros").getBody();
+	        assertNotNull(carros);
+	        assertEquals(30, carros.size());
+	    }
+
+	    @Test
+	    public void testListaPorTipo() {
+
+	        assertEquals(10, getCarros("/api/v1/carros/tipo/classicos").getBody().size());
+	        assertEquals(10, getCarros("/api/v1/carros/tipo/esportivos").getBody().size());
+	        assertEquals(10, getCarros("/api/v1/carros/tipo/luxo").getBody().size());
+
+	        assertEquals(HttpStatus.NO_CONTENT, getCarros("/api/v1/carros/tipo/xxx").getStatusCode());
+	    }
+
+	    @Test
+	    public void testGetOk() {
+
+	        ResponseEntity<CarroDTO> response = getCarro("/api/v1/carros/11");
+	        assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+	        CarroDTO c = response.getBody();
+	        assertEquals("Ferrari FF", c.getNome());
+	    }
+
+	    @Test
+	    public void testGetNotFound() {
+
+	        ResponseEntity response = getCarro("/api/v1/carros/1100");
+	        assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
+	    }
 
 }
